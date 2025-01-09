bb3c20ba8763b2f2ec9e820a49e7c07ebdfd51d7
Chris Ziogas <ziogaschr@gmail.com>
2025-01-09 19:09:37 +0200

    Update git user because of lack of permissions

--- .ci/crawl.sh
+++ .ci/crawl.sh
@@ -2,11 +2,11 @@
 
 devp2p discv4 crawl -timeout "$ETH_DNS_DISCV4_CRAWLTIME" all.json
 
-git add all.json	
+git add all.json
 
 ETH_DNS_DISCV4_KEY_PUBLICINFO="$(cat $ETH_DNS_DISCV4_KEYPASS_PATH | ethkey inspect $ETH_DNS_DISCV4_KEY_PATH | grep -E '(Addr|Pub)')"
-git -c user.name="meows" -c user.email='b5c6@protonmail.com' commit --author 'crawler <>' -m "ci update (all.json) $GITHUB_RUN_ID:$GITHUB_RUN_NUMBER
-        
+git -c user.name="ziogaschr" -c user.email='ziogaschr@gmail.com' commit --author 'crawler <>' -m "ci update (all.json) $GITHUB_RUN_ID:$GITHUB_RUN_NUMBER
+
 Crawltime: $ETH_DNS_DISCV4_CRAWLTIME
 
 $ETH_DNS_DISCV4_KEY_PUBLICINFO"
